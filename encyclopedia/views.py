a244bf746ac30b84143eac754c4db3b5d30038aa
avolent <will@slamped.com>
2022-01-25 22:30:47 +1100

    entry pages and index links working.

--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -1,4 +1,6 @@
+from django.http import HttpResponse
 from django.shortcuts import render
+import markdown2
 
 from . import util
 
@@ -6,5 +8,18 @@ from . import util
 def index(request):
     return render(request, "encyclopedia/index.html", {
         "entries": util.list_entries()
+
     })
 
+def entry(request, entry):
+    if util.get_entry(entry) == None:
+        return render(request, "encyclopedia/error.html", {
+        "error": "Error!",
+        "html": f"Error! The page '{entry}' does not exist on this wiki."
+        })
+    else:
+        html = markdown2.markdown_path(f"entries/{entry}.md")
+        return render(request, "encyclopedia/entry.html", {
+        "entry": entry,
+        "html": html
+        })
