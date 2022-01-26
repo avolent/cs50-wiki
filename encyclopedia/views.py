3d21ca241a4da4f20286e5816ae981ae80036847
avolent <will@slamped.com>
2022-01-26 22:23:58 +1100

    Search funtionality semi working

--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -1,14 +1,15 @@
-from django.http import HttpResponse
+from logging import PlaceHolder
+from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
-import markdown2
+from django.urls import reverse
 
+import markdown2
 from . import util
 
 
 def index(request):
     return render(request, "encyclopedia/index.html", {
-        "entries": util.list_entries()
-
+        "entries": util.list_entries(),
     })
 
 def entry(request, entry):
@@ -21,9 +22,24 @@ def entry(request, entry):
         html = markdown2.markdown_path(f"entries/{entry}.md")
         return render(request, "encyclopedia/entry.html", {
         "entry": entry,
-        "html": html
+        "html": html,
         })
 
 def search(request):
-    print(request)
-    return HttpResponse("test")
+    if request.method == "POST":
+        q = request.POST["q"].lower()
+        entries = util.list_entries()
+        searchlist = []
+        for entry in entries:
+            print(entry)
+            if q == entry.lower():
+                return HttpResponseRedirect(f"wiki/{entry}")
+            elif q in entry.lower():
+                print("Match, adding to search list")
+                searchlist.append(entry)
+                print(searchlist)
+                continue
+        return HttpResponse(f"{searchlist}")
+    else:
+        # Return to index
+        return HttpResponseRedirect(reverse("views:index"))
